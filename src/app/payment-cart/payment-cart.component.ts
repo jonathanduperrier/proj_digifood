7903cfe4305b6e80b9a96d2e7ad6c623d5c3415b
Jonathan DUPERRIER <jonathanduperrier@MacBook-Air-de-Jonathan.local>
2023-10-02 01:03:49 +0200

    cart

--- src/app/payment-cart/payment-cart.component.ts
+++ src/app/payment-cart/payment-cart.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Subscription, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { ProduitsService } from 'src/app/services/produits.service';
 import { ProductCart } from '../models/productcart.model';
@@ -19,12 +21,23 @@ export class PaymentCartComponent {
       qteProd:0
     }
   ];
+  public produits:any;
+  public produitsTab:any;
+  public destroy$: Subject<boolean> = new Subject<boolean>();
 
   ngOnInit(): void {
     this.initData();
   }
   public initData() {
     this.product_cart = this.produitsService.getProductCart();
+    this.produitsService.getListOfProducts().pipe(takeUntil(this.destroy$)).subscribe(
+      (res: any) => {
+        this.produits = res.categories;
+        console.log("this.produits");
+        console.log(this.produits);
+
+      }
+    );
   }
   public pay() {
     this.product_cart = [];
